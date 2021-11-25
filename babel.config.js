fafffa64245ee4579b8e4d74a2af247363d93f7f
dapod93 <davidliois93.konvergen.ai>
2021-11-25 19:57:20 +0700

    update bable config js

--- babel.config.js
+++ babel.config.js
@@ -4,11 +4,14 @@ module.exports = function (api) {
     presets: ["babel-preset-expo"],
   };
 };
-module.exports = {
-  presets: ["module:metro-react-native-babel-preset"],
-  env: {
-    production: {
-      plugins: ["react-native-paper/babel"],
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ["babel-preset-expo"],
+    env: {
+      production: {
+        plugins: ["react-native-paper/babel"],
+      },
     },
-  },
+  };
 };
